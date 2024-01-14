ef27b16fb35c0f22b3560cb39f163ac497e02c47
Samuel Razzaboni <samuel.razzaboni@gmail.com>
2024-01-13 23:27:33 -0300

    Implementação de atributos, construtores e herança

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,60 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+        
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo 
+        {
+            get => _modelo;
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O modelo não pode ser vazio.");
+                }
+                _modelo = value;
+            }
+        }
+        public string IMEI
+        {
+            get => _imei;
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O IMEI não pode ser vazio.");
+                }
+                _imei = value;
+            }
+        }
+        public int Memoria
+        {
+            get => _memoria;
+            set
+            {
+                if(value == 0)
+                {
+                    throw new ArgumentException("A memória não pode ser vazio.");
+                }
+                _memoria = value;
+            }
+        }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+
+        
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Nokia nokia = new Nokia("986229441", "MiA3", "65432198765321415", 64);
+       Iphone iphone = new Iphone("946213569", "14 pro-Max", "654321789453168", 128);
+
+
+
+       
+    }
+}
